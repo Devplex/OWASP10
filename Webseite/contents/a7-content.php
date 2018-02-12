e112acbd9d0bd741e4cee88ec3617aeb5346ea8d
Daniel <DanielSenpai95@gmail.com>
2018-02-12 20:21:29 +0100

    Videos zu allen Seiten hinzugefügt.

--- Webseite/contents/a7-content.php
+++ Webseite/contents/a7-content.php
@@ -9,7 +9,7 @@
  *****************************************************************************/
  ?>
 <div class="w3-main" style="margin-left:250px">
-
+  <!-- Inhalt -->
   <div class="w3-row w3-padding-64">
     <div class="w3-twothird w3-container">
       <h1 class="w3-text-teal">Crosside-Scripting (XSS)</h1>
@@ -46,7 +46,29 @@
       <img src="../res/img/owasp-logo-negative.png" alt="OWASP Logo" height="40%" width="100%">
     </div>
   </div>
-
+  <!-- Video -->
+  <div class="w3-row">
+    <div class="w3-twothird w3-container">
+      <h3>Video zum Angriff</h3>
+      <p>Super wichtig aussehender Text der richtig interessante Informationen.
+        Beinhaltet die einen so richtig weiter bringen im Leben!</p>
+      <video style="margin-left:auto;margin-right:auto;" width="320" height="240" controls>
+        <source src="../res/videos/A7-Cross-Site Scripting.mp4" type="video/mp4">
+        Dein Browser unterstürtzt keine Videos!
+      </video>
+    </div>
+  </div>
+  <!-- Quiz -->
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h3>Teste dich selbst!</h3>
+      <p>Mit dem Quiz kannst du Testen ob du auch wirklich alles Verstanden
+        hast!</p>
+      <button class="w3-button w3-left w3-teal"
+        onclick="location.href='../templates/quiz-template.php?quiz=quiz-test'">
+        Quiz starten!</button>
+    </div>
+  </div>
+  <!-- Footer -->
   <?php include '../parts/footer.php'; ?>
-
 </div>
